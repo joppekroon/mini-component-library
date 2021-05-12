bed486a20411c8e93b0161ec49be84ea720831d8
Joppe Kroon <J.Kroon@topdesk.com>
2021-05-12 12:02:33 +0200

    Select implementation

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,54 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children} 
+      </NativeSelect>
+      <span className="value">{displayedValue}</span>
+      <Icon className="icon" id="chevron-down" size="24" strokeWidth="2"></Icon>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: inline-block;
+  position: relative;
+  
+  padding: 8px 16px 9px 16px;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  
+  color: ${COLORS.gray700};
+  
+  &:hover {
+    color: ${COLORS.black};
+  };
+  
+  &:focus-within {
+    outline: auto;
+  }
+  
+  .icon {
+    display: inline-block;
+    vertical-align: middle;
+    margin-left: 24px;
+    margin-top: 2px;
+  }
+  
+  .value {
+    vertical-align: middle;
+  }
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  
+  opacity: 0;
+`;
 
 export default Select;
